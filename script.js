7a9332c56ae90f6a0c5d5556a9553bbb26e261f5
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-25 22:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-cy.get('li').should('have.length', 5);
-
-const paths = ['Cloud', 'Desktop', 'Video Game', 'Mobile', 'Web']; // corrected 'Video Sport' to 'Video Game'
-for (let index = 0; index < paths.length; index++) {
-  cy.get('li').eq(index).should('have.text', `${paths[index]} development`);
-}
+const paths = ['Cloud', 'Desktop', 'Video Game', 'Mobile', 'Web'];
